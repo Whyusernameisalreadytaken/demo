9df5aebf3c071c785d737770efa505539972edc5
weiliming <weiliming_ok@163.com>
2018-09-15 13:20:54 +0800

    multi env config

--- src/main/java/com/example/demo/web/HelloController.java
+++ src/main/java/com/example/demo/web/HelloController.java
@@ -1,13 +1,22 @@
 package com.example.demo.web;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    @Value("${random.string}")
+    private String randomString;
+
     @RequestMapping("/hello")
     public String index(){
         return "Hello World";
+    }
+
+    @RequestMapping("/randomString")
+    public String randdomString(){
+        return randomString;
     }
 }
