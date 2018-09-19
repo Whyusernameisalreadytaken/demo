8ce3ccba1d056e38c3f0552bed2d5baa3ca285f5
weiliming <weiliming_ok@163.com>
2018-09-19 19:44:48 +0800

    img download

--- src/main/java/com/example/demo/web/HelloController.java
+++ src/main/java/com/example/demo/web/HelloController.java
@@ -1,11 +1,21 @@
 package com.example.demo.web;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.util.Base64Utils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.Base64;
+
 @RestController
 public class HelloController {
+
+    private static final String DATA_IMG_PREFIX = "data:image/jpeg;base64,";
 
     @Value("${random.string}")
     private String randomString;
@@ -23,6 +33,25 @@ public class HelloController {
     @RequestMapping("/springApplicationJson")
     public String springApplicationJson(){
         return System.getProperty("SPRING_APPLICATION_JSON");
+    }
+
+    @RequestMapping("/img")
+    public String img() throws IOException {
+        File file = new File("/Users/yingbaby/Downloads/车牌.JPG");
+        FileInputStream inputStream = new FileInputStream(file);
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+
+        int len;
+        byte[] bytes = new byte[1024];
+        while ((len = inputStream.read(bytes)) != -1){
+            outputStream.write(bytes,0,len);
+        }
+        byte[] res = outputStream.toByteArray();
+
+        outputStream.close();
+        inputStream.close();
+        Base64.Encoder encoder = Base64.getMimeEncoder();
+        return DATA_IMG_PREFIX+encoder.encodeToString(res);
     }
 
 }
